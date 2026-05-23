cc7f36d133e19b980c25a6347391fc9d69ac7a51
iFelice <manganiellofelice@gmail.com>
2026-05-23 13:17:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ st.markdown(f"""
     html, body, [data-testid="stapp"] {{ background-color: {bg} !important; color: {txt} !important; font-family: 'Inter', sans-serif; }}
     .stApp {{ background-color: {bg}; }}
              .banner-fullwidth {{
-        margin: -50px -40px 00px -40px;
+        margin: -90px -80px 40px -80px;
         overflow: hidden;
         border-radius: 0 0 20px 20px;
         box-shadow: 0 4px 15px rgba(0,0,0,0.3);
